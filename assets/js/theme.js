996e9e8ca0b5da54d5e8a693fd47eb10e2c1bd45
Nils Fürniß <contact@nalsai.de>
2024-06-30 15:11:22 +0900

    Load and execute theming js synchronously to prevent color changes/flashes while loading

---
+++ assets/js/theme.js
@@ -0,0 +1,118 @@
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+
+    // if currently on localhost, domain doesn't need to be set
+    if (location.hostname === "localhost") {
+        document.cookie = name + "=" + (value || "") + expires + ";path=/;SameSite=Strict";
+        return;
+    }
+    const domain = "; domain=." + location.hostname.split('.').reverse()[1] + "." + location.hostname.split('.').reverse()[0];
+    console.log(domain);
+    document.cookie = name + "=" + (value || "") + expires + ";path=/" + domain + ";SameSite=Srict;secure";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+// Theme switcher
+(() => {
+    'use strict'
+
+    //const getStoredTheme = () => localStorage.getItem('theme')
+    //const setStoredTheme = theme => localStorage.setItem('theme', theme)
+    // alternative with cookies
+    const getStoredTheme = () => getCookie('theme')
+    const setStoredTheme = theme => setCookie('theme', theme, 400);
+
+    const getPreferredTheme = () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme) {
+            return storedTheme
+        }
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    const setTheme = theme => {
+        if (theme === 'auto') {
+            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+
+        var x = document.getElementsByClassName("invert-light");
+        for (var counter = 0; counter < x.length; counter++) {
+            if (theme === 'auto') {
+                x.item(counter).style = window.matchMedia('(prefers-color-scheme: dark)').matches ? '' : 'filter: invert(1)'
+            }
+            else {
+                x.item(counter).style = theme === 'light' ? 'filter: invert(1)' : ''
+            }
+        }
+    }
+
+    setTheme(getPreferredTheme())
+
+    const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector('#bd-theme')
+
+        if (!themeSwitcher) {
+            return
+        }
+
+        const themeSwitcherText = document.querySelector('#bd-theme-text')
+        const activeThemeIcon = document.querySelector('.theme-icon-active')
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+        const iOfActiveBtn = btnToActive.querySelector('i').getAttribute('class')
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+            element.classList.remove('active')
+            element.setAttribute('aria-pressed', 'false')
+        })
+
+        btnToActive.classList.add('active')
+        btnToActive.setAttribute('aria-pressed', 'true')
+        activeThemeIcon.setAttribute('class', iOfActiveBtn + ' theme-icon-active')
+        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
+        themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
+
+        if (focus) {
+            themeSwitcher.focus()
+        }
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme !== 'light' && storedTheme !== 'dark') {
+            const theme = getPreferredTheme()
+            setTheme(theme)
+            showActiveTheme(theme, false);
+        }
+    })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]')
+            .forEach(toggle => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value')
+                    setStoredTheme(theme)
+                    setTheme(theme)
+                    showActiveTheme(theme, true)
+                })
+            })
+    })
+})();
